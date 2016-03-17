42f917afba2b7f8753435d3b38be8e589d35b79d
awbirdsall <abirdsall@gmail.com>
2016-03-17 09:09:55 -0400

    3.05 working calls to random and estimate-circle-area

--- sicp_ex_3_05.rkt
+++ sicp_ex_3_05.rkt
@@ -1,4 +1,10 @@
 #lang planet neil/sicp
+; random in neil/sicp doesn't seem to return decimal value
+; for decimal argument, as sicp claims is the case for MIT
+; Scheme. Instead, use racket/base random, which returns 
+; random number between 0 and 1 (exclusive) when called
+; without argument.
+(#%require (only racket/base random))
 
 (define (monte-carlo trials experiment)
   (define (iter trials-remaining trials-passed)
@@ -12,7 +18,7 @@
 
 (define (random-in-range low high)
   (let ((range (- high low)))
-    (+ low (random range))))
+    (+ low (* (random) range))))
 
 ; function that returns different random result each time 
 (define p1
@@ -31,14 +37,23 @@
         (x-high (+ x r))
         (y-low (- y r))
         (y-high (+ y r)))
-    (define make-circle-predicate-tight
-      (lambda ()
-        (<= (+ (expt (- (random-in-range x-low x-high) x) 2)
-               (expt (- (random-in-range y-low y-high) y) 2))
-            (expt r 2))))
-    (estimate-integral (make-circle-predicate-tight)
+    ; circle-predicate nested in lambda so that it acts as
+    ; monte-carlo expects `experiment` to behave: procedure
+    ; that takes no arguments and returns a Boolean
+    ; dependent on a re-roll of random each time.
+    (estimate-integral (lambda () ((circle-predicate) x y r))
                        x-low
                        x-high
                        y-low
                        y-high
                        trials)))
+
+(define (circle-predicate)
+  (lambda (x y r)
+    (let ((x-low (- x r))
+          (x-high (+ x r))
+          (y-low (- y r))
+          (y-high (+ y r)))
+      (<= (+ (expt (- (random-in-range x-low x-high) x) 2)
+             (expt (- (random-in-range y-low y-high) y) 2))
+          (expt r 2)))))
