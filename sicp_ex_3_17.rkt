d784debc5eb9ca8e6bff508e8af1c0929606206d
awbirdsall <abirdsall@gmail.com>
2016-04-21 08:54:54 -0400

    3.17 comment identifying source of side-effect bug

--- sicp_ex_3_17.rkt
+++ sicp_ex_3_17.rkt
@@ -4,8 +4,9 @@
       x
       (last-pair (cdr x))))
 (define (my-append! x y)
-  (set-cdr! (last-pair x) y)
-  x)
+  (begin
+    (set-cdr! (last-pair x) y)
+    x))
 (define (is-member? x l)
   (cond ((null? l) #f)
         ((eq? x (car l)) #t)
@@ -14,13 +15,20 @@
   (define (count-pairs-aux x aux)
     (cond ((not (pair? x)) 0)
           ((not (is-member? x (cdr aux)))
-           (my-append! aux x)
-           (+ (count-pairs-aux (car x) aux)
-              (count-pairs-aux (cdr x) aux)
-              1))
+           (begin
+             ; this is problem! since `last-pair` of `aux` points to
+             ; elements of the `x` whose pairs are counted, `my-
+             ; append!` modifies not just `aux` but also `x`!
+             (my-append! aux x)
+             (display aux)
+             (+ (count-pairs-aux (car x) aux)
+                (count-pairs-aux (cdr x) aux)
+                1)))
           (else
-           (my-append! aux x)
-           (+ (count-pairs-aux (car x) aux)
-              (count-pairs-aux (cdr x) aux)
-              0))))
+           (begin
+             (my-append! aux x)
+             (display aux)
+             (+ (count-pairs-aux (car x) aux)
+                (count-pairs-aux (cdr x) aux)
+                0)))))
   (count-pairs-aux x '(dummy)))
