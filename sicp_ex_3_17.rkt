b9f7b4bc5a365c5e4736297d1053fcb31790e94e
awbirdsall <abirdsall@gmail.com>
2016-04-22 09:14:53 -0400

    3.17 fully working

--- sicp_ex_3_17.rkt
+++ sicp_ex_3_17.rkt
@@ -3,10 +3,6 @@
   (if (null? (cdr x))
       x
       (last-pair (cdr x))))
-(define (my-append! x y)
-  (begin
-    (set-cdr! (last-pair x) y)
-    x))
 (define (is-member? x l)
   (cond ((null? l) #f)
         ((eq? x (car l)) #t)
@@ -14,21 +10,17 @@
 (define (count-pairs x)
   (define (count-pairs-aux x aux)
     (cond ((not (pair? x)) 0)
-          ((not (is-member? x (cdr aux)))
-           (begin
-             ; this is problem! since `last-pair` of `aux` points to
-             ; elements of the `x` whose pairs are counted, `my-
-             ; append!` modifies not just `aux` but also `x`!
-             (my-append! aux x)
-             (display aux)
-             (+ (count-pairs-aux (car x) aux)
-                (count-pairs-aux (cdr x) aux)
-                1)))
+          ((not (is-member? x aux))
+           (if (null? (car aux))
+               (set-car! aux x) ; should only be first pair encountered,
+               ; check above for `not pair?` should prevent issue with
+               ; (car aux) ever returning to null.
+               (set-cdr! (last-pair aux) (cons x '())))
+           (+ (count-pairs-aux (car x) aux)
+              (count-pairs-aux (cdr x) aux)
+              1))
           (else
-           (begin
-             (my-append! aux x)
-             (display aux)
-             (+ (count-pairs-aux (car x) aux)
-                (count-pairs-aux (cdr x) aux)
-                0)))))
-  (count-pairs-aux x '(dummy)))
+           (+ (count-pairs-aux (car x) aux)
+              (count-pairs-aux (cdr x) aux)
+              0))))
+  (count-pairs-aux x (cons '() '())))
