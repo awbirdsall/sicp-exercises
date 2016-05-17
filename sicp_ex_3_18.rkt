0ec875ca8e1d26729f55b74ff535e14f0ca6bd33
awbirdsall <abirdsall@gmail.com>
2016-05-17 08:53:53 -0400

    3.18 working has-cycle?
    Change to pure function without set-car! or set-cdr!

--- sicp_ex_3_18.rkt
+++ sicp_ex_3_18.rkt
@@ -7,20 +7,18 @@
   (if (null? (cdr x))
       x
       (last-pair (cdr x))))
+
 (define (has-cycle? x)
+  ; Search down each branch until reach leaf, or something
+  ; previously encountered on same branch.
+  ; More general search for cycle than "whether a program that
+  ; tried to find the end of the list by taking successive cdrs
+  ; would go into an infinite loop" in exercise statement --
+  ; can have structure where cdrs terminate, but cycle in cars.
   (define (has-cycle-aux x aux)
     (cond ((not (pair? x)) #f)
           ((is-member? x aux) #t)
           (else
-           (if (null? (car aux))
-               (set-car! aux x)
-               (set-cdr! (last-pair aux) (cons x '())))
-           (display aux)
-           ; problem: search for cycle down (car x) branch
-           ; adds to aux that is then used during the search
-           ; down the (cdr x) branch -- leads to false positives
-           (or (has-cycle-aux (car x) aux)
-               (has-cycle-aux (cdr x) aux)))))
-    (has-cycle-aux x (cons '() '())))
-  
-  
+           (or (has-cycle-aux (car x) (cons x aux))
+               (has-cycle-aux (cdr x) (cons x aux))))))
+  (has-cycle-aux x '()))
